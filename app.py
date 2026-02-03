2ff4e35ab7ffe60a7fd970fbcfb04032f7ec0585
kabeerahmead6-commits <kabeerahmead6@gmail.com>
2026-02-04 00:16:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import hashlib
 import json
 from time import time
-from flask import Flask, jsonify
-from flask_cors import CORS  # இதைச் சேர்த்துள்ளோம்
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
+        # Genesis block
         self.new_block(previous_hash="1", proof=100)
 
     def new_block(self, proof, previous_hash=None):
@@ -22,13 +23,25 @@ class Blockchain:
         self.chain.append(block)
         return block
 
+    def new_transaction(self, sender, recipient, amount):
+        self.pending_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     @staticmethod
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
 app = Flask(__name__)
-CORS(app) # இதுதான் MeDo ஆப்-ஐ உங்கள் சர்வருடன் பேச அனுமதிக்கும்
+CORS(app) # MeDo-வுடன் இணைக்க இது கட்டாயம்!
 blockchain = Blockchain()
 
 @app.route('/chain', methods=['GET'])
@@ -37,11 +50,20 @@ def full_chain():
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    last_block = blockchain.chain[-1]
+    last_block = blockchain.last_block
     proof = last_block['proof'] + 1
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
     return jsonify(block), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    return jsonify({'message': f'Transaction will be added to Block {index}'}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
